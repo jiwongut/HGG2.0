242a8834a97f50e03c67e65ad956d962dd7b18ae
SeolHuiGwan9478 <shg5977@naver.com>
2022-01-31 21:24:01 +0900

    initialize project folders

--- backend/hgg_project/hgg_project/wsgi.py
+++ backend/hgg_project/hgg_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hgg project.
+WSGI config for hgg_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hgg.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hgg_project.settings')
 
 application = get_wsgi_application()

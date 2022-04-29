10e58549c0c7c0e2599a0ee49bbab3d65c0097b3
Parkjju <rudwns3927@gmail.com>
2022-04-29 18:02:19 +0900

    [Add] FE project init

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import Header from './App';
+
+function App() {
+  return (
+    <div className='App'>
+      <p>Hello</p>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Header.js
@@ -0,0 +1,5 @@
+function Header() {
+  return <div>Hello</div>;
+}
+
+export default Header;

4bad83870331d1812593637779432c2ae67773a7
Jinhyung Rhee <rheejinhyung@gmail.com>
2022-07-08 17:47:53 +0900

    [refactor] backend 디렉토리 수정

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,4 +1,4 @@
-"""hgg_project URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -13,19 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import imp
-from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hgg.urls')),
-    # user autentication
-    path(
-        "email-confirmation-required/",
-        TemplateView.as_view(template_name = "hgg/email-confirmation-required.html"),
-        name = "account_email_confirmation_required",
-    ),
 ]

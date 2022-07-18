58fa6fb12bcd49c78af47cac9d11668d1061d743
jiwongut <wony4094@naver.com>
2022-07-18 21:55:09 +0900

    0718

--- frontend/src/routes/review/reviewPost.js
+++ frontend/src/routes/review/reviewPost.js
@@ -1,4 +1,20 @@
+import Backward from '../../components/Backward';
+// import PostButton from '../../components/Button';
+import Container from '../../components/Container';
+// import BackwardTitle from '../Mypage/Home/BackwardTitle';
+import BackwardTitleBox from '../Mypage/Home/BackwardTitleBox';
+import Header from '../../components/Header';
+import Navigation from '../../components/Navigation';
+import CategoryResult from '../Category';
+
 function ReviewPost() {
-  return null;
-}
+  return (
+    <Container>
+      <Header />
+      <BackwardTitleBox title="후기게시판" />
+      <Navigation />
+    </Container>
+  );
+  }
+
 export default ReviewPost;
